c2753e8ec28c1f39cb241f222c414940dd033864
Gaurav Bora <160889162+Gaurav22020@users.noreply.github.com>
2026-02-16 18:18:44 +0530

    Change server listen address to 0.0.0.0

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.get('/api/rates', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on 0.0.0.0:${PORT}`);
 });
